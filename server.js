5a487facf87700da6828f3e40776fd35b49cdc45
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-15 18:49:21 +0530

    init

---
+++ server.js
@@ -0,0 +1,39 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express')
+const app = express()
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + '/views/index.html')
+})
+
+// Simple in-memory store
+const dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+]
+
+app.get("/dreams", (request, response) => {
+  response.send(dreams)
+})
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", (request, response) => {
+  dreams.push(request.query.dream)
+  response.sendStatus(200)
+})
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+})
