e951a1f6f834b4aac02446bf01610e8808836a4b
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2018-07-16 10:08:49 +0530

    add fonts

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.post("/dreams", (request, response) => {
 })
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
+const listener = app.listen(3000, () => {
+  console.log(`Your app is listening on port 3000`)
 })
