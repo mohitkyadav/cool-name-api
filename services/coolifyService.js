5da9db28006e4e146e16d98c889cbbac1817ab17
Anish Sujanani <ansujanani@hotmail.com>
2018-10-03 13:52:30 +0530

    Added another dict, modified services and server (#4)
    * Added another dict, modified services and server
    
    * modified dict
    
    * Shifted indices in server

--- services/coolifyService.js
+++ services/coolifyService.js
@@ -73,6 +73,14 @@ coolifyDicts.rounded = {
   'z' : 'ⓩ'
 };
 
+coolifyDicts.symbolic = {
+  'a': '@',
+  'c': '(',
+  'i': '!',
+  'l': '|',
+  't': '7'
+}
+
 String.prototype.allReplace = function(obj) {
   var retStr = this;
   for (var x in obj) {
@@ -89,7 +97,8 @@ coolify.alphaNumericName = function(name) {
 		let cool_name_square = name.allReplace(coolifyDicts.square);
 		let cool_name_round_alphanum = cool_name_rounded.slice(0, 1) + cool_name_alphanum.slice(1, name.length - 1)
 												+ cool_name_rounded.slice(name.length - 1, name.length);
-    return [cool_name_alphanum, cool_name_rounded, cool_name_square, cool_name_round_alphanum];
+   		let cool_name_symbolic = name.allReplace(coolifyDicts.symbolic);
+     return [cool_name_alphanum, cool_name_rounded, cool_name_square, cool_name_round_alphanum, cool_name_symbolic];
   } else {
     return false;
   }
