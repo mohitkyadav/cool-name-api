6fb7d8ba70581a02c87f8c093ef1b9c89ed9f0f2
dpritos <d.pritos@arcanys.com>
2018-10-03 16:25:59 +0800

    additional algo

--- services/coolifyService.js
+++ services/coolifyService.js
@@ -88,8 +88,10 @@ coolify.alphaNumericName = function(name) {
     let cool_name_rounded = name.allReplace(coolifyDicts.rounded);
 		let cool_name_square = name.allReplace(coolifyDicts.square);
 		let cool_name_round_alphanum = cool_name_rounded.slice(0, 1) + cool_name_alphanum.slice(1, name.length - 1)
-												+ cool_name_rounded.slice(name.length - 1, name.length);
-    return [cool_name_alphanum, cool_name_rounded, cool_name_square, cool_name_round_alphanum];
+                        + cool_name_rounded.slice(name.length - 1, name.length);
+    let cool_name_center_round = cool_name_square.slice(0, 1) + cool_name_rounded.slice(1, name.length -1 )
+												+ cool_name_square.slice(name.length - 1, name.length);
+    return [cool_name_alphanum, cool_name_rounded, cool_name_square, cool_name_round_alphanum, cool_name_center_round];
   } else {
     return false;
   }
